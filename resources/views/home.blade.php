eeb5ac8377d276d1f79ad33e0b222d455adf8e20
Jonathan Jean Lemke <mild.jonathan@gmail.com>
2023-08-09 06:50:39 -0300

    Finished quickly creating a home page to fill empty space

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,36 @@
                     @endif
 
                     {{ __('You are logged in!') }}
+
+                    <!-- two columns cards -->
+                    <!-- the cards have a image to represent the area -->
+                    <!-- the cards have a title -->
+                    <!-- the cards have a description -->
+                    <!-- the cards link to route list of especific area -->
+                    <div class="row justify-content-center">
+                        <div class="col-md-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <img src="{{ asset('school_icon.jpeg') }}" class="card-img-top" alt="...">
+                                    <h5 class="card-title"><a href="{{ route('schools.index') }}"> Schools </a></h5>
+                                    <p class="card-text"> Ver la lista de escuelas </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <img src="{{ asset('student_icon.jpeg') }}" class="card-img-top" alt="...">
+                                    <h5 class="card-title"><a href="{{ route('students.index') }}"> Students </a></h5>
+                                    <p class="card-text"> Ver la lista de alumnos </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
                 </div>
             </div>
         </div>
